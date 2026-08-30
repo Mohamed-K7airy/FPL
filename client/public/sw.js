7aa3c5ffbfe713ad0f4805558342a3550c1bed43
Mohamed-K7airy <mohamedkh476@gmail.com>
2026-08-30 20:40:54 +0300

    add : push

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11682219
+    "zoneId": 11689732
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
