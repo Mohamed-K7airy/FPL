a0f0c8a1e334bb8b67141cea0c0fed5b17ca2838
Mohamed-K7airy <mohamedkh476@gmail.com>
2026-08-30 20:58:07 +0300

    update: push tag to 11689844

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11689732
+    "zoneId": 11689844
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
